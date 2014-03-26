c144febac3c9feafe9f2b8c1d6dab40706356420
Luis Nicanor <luisnicg@MacBook-Air-de-Luis.local>
2014-03-25 23:32:29 -0600

    The description

--- form/connect.php
+++ form/connect.php
@@ -2,6 +2,6 @@
 	include "../classes/cbSQLConnect.class.php";
 	date_default_timezone_set("America/Mexico_City");
 	
-	$databaseSetup = new cbSQLConnectConfig( cbSQLConnectVar::DB_MYSQL, "localhost","3306","mybookmarks","root","arquitectura2"); 
+	$databaseSetup = new cbSQLConnectConfig( cbSQLConnectVar::DB_MYSQL, "localhost","3306","mybookmarks","root",""); 
 
 ?>
